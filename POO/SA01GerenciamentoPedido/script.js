afce514b08e4c6b07825cb94ec7dbbf52324b4e5
Diogotb <digogo@gmail.com>
2025-04-17 11:57:45 -0300

    17/04

--- POO/SA01GerenciamentoPedido/script.js
+++ POO/SA01GerenciamentoPedido/script.js
@@ -1,6 +1,6 @@
 //criar as classes
 
-class Cliente {
+class Cliente { //model
   #id;
   #nome;
   constructor(id, nome) {
@@ -16,7 +16,7 @@ class Cliente {
   }
 }
 
-class Produto {
+class Produto { //model
   #id;
   #nome;
   #preco;
@@ -36,7 +36,7 @@ class Produto {
   }
 }
 
-class Pedido {
+class Pedido { //model
   #id;
   #cliente;
   #itens;
@@ -51,7 +51,7 @@ class Pedido {
   }
   calcularTotal() {
     let total = this.#itens.reduce(
-      (acc, item) => acc + item.produto.getPreco() * item.quantidade,
+      (acc, item) => acc + item.preco * item.quantidade,
       0
     );
     return total - total * (this.#desconto / 100);
@@ -74,7 +74,7 @@ class Pedido {
   }
 }
 
-class SistemaPedidos {
+class SistemaPedidos {  //controller (crud)
     #clientes;
     #produtos;
     #pedidos;
@@ -84,8 +84,19 @@ class SistemaPedidos {
         this.#pedidos = [];
     }
 
+    // getters
+    getClientes(){
+        return this.#clientes;
+    }
+    getProdutos(){
+      return this.#produtos;
+    }
+    getPedidos(){
+      return this.#pedidos;
+    }
+
     cadastrarCliente(){
-        const nome = document.getElementById("produtoNome").value;
+        const nome = document.getElementById("clienteNome").value;
         if(!nome) return alert("Digite un Nome para o Cliente.");
         const cliente = new Cliente(this.#clientes.length+1, nome);
         this.#clientes.push(cliente);
@@ -115,6 +126,99 @@ class SistemaPedidos {
 
     //lista de produtos
 
+    cadastrarProduto(){
+        const nome = document.getElementById("produtoNome").value;
+        const preco = document.getElementById("produtoPreco").value;
+        if(!nome || isNaN(preco)) return alert("Preencha os campos corretamente.");
+        //instanciar um objeto da classe produto
+        const produto = new Produto(this.#produtos.length+1, nome, preco);
+        this.#produtos.push(produto);
+        this.atualizarProdutos(); // método para atualizar a lista de produtos no HTML
+        document.getElementById("produtoNome").value = "";
+        document.getElementById("produtoPreco").value = "";
+    }
 
+    atualizarProdutos(){
+        const lista = document.getElementById("listaProdutos");
+        lista.innerHTML = "";
+
+        const produtoDiv = document.getElementById("produtosDisponiveis");
+        produtoDiv.innerHTML = "";
+
+        //percorrer a lista de produtos e preencher os elementos
+        this.#produtos.forEach( produto =>{
+            const li = document.createElement("li");
+            li.textContent = `${produto.getNome()} - R$ ${produto.getPreco()}`;
+            lista.appendChild(li); //produto na lista de proutos
+
+            const checkbox = document.createElement("input");
+            checkbox.type = "checkbox";
+            checkbox.value = produto.getId();
+
+            const label = document.createElement("label");
+            label.textContent = `${produto.getNome()} - R$ ${produto.getPreco()}`;
+
+            const inputQuantidade = document.createElement("input");
+            inputQuantidade.type = "number";
+            inputQuantidade.value = 1;
+            inputQuantidade.min = 1;
+            //produtos na lista de pedidos
+            produtoDiv.appendChild(checkbox);
+            produtoDiv.appendChild(label);
+            produtoDiv.appendChild(inputQuantidade);
+            produtoDiv.appendChild(document.createElement("br"));
+            checkbox.addEventListener("change", () =>{
+                if(checkbox.checked){
+                  inputQuantidade.disabled = false;
+                }else{
+                  inputQuantidade.disabled = true;
+                }
+            });
+        });
+    }
+  
     //lista de pedidos
+    finalizarPedido(){
+        const clienteNome = document.getElementById("selectCliente").value;
+        if(!clienteNome)return alert("Selecione um cliente.");
+        const cliente = this.#clientes.find( c => c.getId() == clienteNome);
+
+        const desconto = parseFloat(document.getElementById("desconto").value) || 0;
+        const itens = [];
+        document.querySelectorAll("#produtosDisponiveis input[type='checkbox']").forEach((checkbox,index) => {
+            if(checkbox.checked){
+                const produtoId = parseInt(checkbox.value);
+                const produto = this.#produtos.find( p => p.id === produtoId);
+                const quantidade = parseInt(document.querySelectorAll("#produtosDisponiveis input[type = 'number']")[index].value);
+                itens.push({produto, quantidade});
+            }
+        });
+        if(itens.length === 0) return alert("Selecione pelo menos um Produto.");
+        //instanciar um Pedido
+        const pedido = new Pedido(this.#pedidos.length+1, cliente, itens, desconto);
+        this.#pedidos.push(pedido);
+        this.atualizarPedidos(); // método para atualizar os pedidos no HTML
+        //limpa os campos
+        document.getElementById("selectCliente").value = "";
+        document.getElementById("desconto").value = "";
+        document.querySelectorAll("#produtosDisponiveis input[type='checkbox']").forEach( checkbox => {
+          checkbox.checked = false;
+        });
+    }
+
+    atualizarPedidos(){
+      const lista = document.getElementById("listaPedidos");
+      lista.innerHTML = "";
+
+      //percorrer a lista de pedidios e preencher os elementos
+      this.#pedidos.forEach( pedido => {
+          const li = document.createElement("li");
+          li.textContent = `Pedido ID: ${pedido.getId()} - Cliente: ${pedido.getCliente().getNome()} - Total: R$ ${pedido.item.getTotal()}`;
+          lista.appendChild(li);
+      });
+    }
 }
+
+//instanciar no sistema de Pedidos
+
+const sistema = new SistemaPedidos();
